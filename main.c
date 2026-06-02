96837546d4891fcf8f61e3de79ede7891ab1f8dd
henrique0rodrigues <henriqueorodriguez13@gmail.com>
2026-06-02 11:33:44 -0300

    Versão funcional do jogogit add .

--- main.c
+++ main.c
@@ -2,21 +2,25 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdbool.h>
+#include <conio.h>
 
-typedef struct node
+typedef struct elem
 {
+    int id;
     bool hand;
-    struct nodo *next;
-};
+    struct elem *link;
+}celula;
 
-typedef struct node *lista_C;
 
 int main()
 {
-    srand(time(NULL));
-    int menu;
-    int tempo_aleatorio = (rand() % 6) + 5;
+    celula j1,j2, j3;
+    celula *atual = NULL;
+    
+    int menu, sorteado;
 
+    srand(time(NULL));
+    int tempo_aleatorio = (rand() % 6) + 6;
     time_t tempo_inicio = time(NULL);
     time_t tempo_atual;
 
@@ -28,24 +32,70 @@ int main()
         printf("Digite 2 para partida com tres jogadores\n");
         printf("Escolha: ");
         scanf("%d", &menu);
+        system("cls");
     } while (menu < 1 || menu > 2);
 
-    switch (menu)
-    {
-    case 1:
-        // cria lista circular com duas posições
-        break;
-    case 2:
-        // cria lista circular com três posições
-        break;
-    }
+    j1.id = 1; j2.id = 2; j3.id = 3;
+    j1.hand = false; j2.hand = false; j3.hand = false;
 
-    while (true)
-    {
+    switch (menu){
+        case 1:
+            j1.link = &j2;
+            j2.link = &j1;
+            
+            sorteado = rand() % 2;
+            if(sorteado == 0){
+                j1.hand = true;
+                atual = &j1;
+            } else{
+                j2.hand = true;
+                atual = &j2;
+            }   
+            break;
+        case 2:
+
+            j1.link = &j2;
+            j2.link = &j3;
+            j3.link = &j1;
+
+            sorteado = rand() % 3;
+            if(sorteado == 0){
+                j1.hand = true;
+                atual = &j1;
+            } else if (sorteado == 1){
+                j2.hand = true;
+                atual = &j2;
+            } else{
+                j3.hand = true;
+                atual = &j3;
+            }
+            break;
+    }
+    printf("\n--- O JOGO COMECOU!! ---\n");
+    printf("A batata esta como jogador %d!\n", atual->id);
+    printf("Para passar a batata pressione ESPACO  o quanto antes!!!\n");
+
         do
         {
             tempo_atual = time(NULL);
 
+            if (_kbhit()) {
+            // Lê a tecla pressionada sem precisar apertar Enter
+            char tecla = _getch(); 
+
+            // Verifica se a tecla pressionada foi o espaço
+            if (tecla == ' ') {
+                atual->hand = false;
+                atual = atual->link;
+                atual->hand = true;
+                
+                printf("A batata foi passada!! Agora esta com o jogador %d\n", atual->id);
+            }
+        }
         } while (difftime(tempo_atual, tempo_inicio) < tempo_aleatorio);
-    }
+    
+    printf("\n FIM DE JOGO! O tempo acabou!!\n");
+    printf("O jogador %d estava com a batata na mao e perdeu!!\n", atual->id);
+
+    return 0;
 }
